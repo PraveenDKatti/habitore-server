d5201062c0e83e646f5c6baab3bc290adcd5bafe
Praveen Katti <praveendk25@gmail.com>
2026-07-19 15:31:36 +0530

    refactor: cleared files and updated with new file structure.

--- src/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d', // Token is valid for 30 days
-  });
-};
-
-export default generateToken;
